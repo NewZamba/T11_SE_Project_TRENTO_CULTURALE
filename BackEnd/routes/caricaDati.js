ff5dd8a9f8258c43dd3d73a46141e14b5e03bf71
asassingio <75342393+asassingio@users.noreply.github.com>
2024-11-27 14:01:52 +0100

    aggiunto env e modificato il gitignore

--- BackEnd/routes/caricaDati.js
+++ BackEnd/routes/caricaDati.js
@@ -2,16 +2,18 @@ var express = require('express');
 var router = express.Router();
 const Events = require('../models/Events');
 const mongoose = require("mongoose");
+require('dotenv').config();
 
 /* GET users listing. */
 router.get('/', function(req, res, next)
 {
-    mongoose.connect('mongodb+srv://fraCok:fraCok@cluster0.c9u75.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
+    const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.c9u75.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
             console.log("Connessione al database riuscita");
             // Creazione di un oggetto fittizio
             const fakeEvent = new Events({
-                id_event: 101,
+                id_event: 404,
                 name_event: "Workshop Tecnologico",
                 location_event: "Roma, Centro Congressi",
                 date_event: new Date('2024-12-20'),
