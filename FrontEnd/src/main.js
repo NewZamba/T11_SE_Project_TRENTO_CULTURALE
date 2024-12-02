4f5a0545ba609a9bb6c0c8e2241f831ad83935bb
Francesco-Buscardo <francescobuscardo@gmail.com>
2024-12-02 16:59:44 +0100

    Problems

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './components/App.vue';
+import '../index.css';
+import BootstrapVue from 'bootstrap-vue';
+import { IconsPlugin } from 'bootstrap-vue';
+import router from './router.js';
 
-createApp(App).mount('#app')
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+new Vue({
+    router,  // Collega il router all'app
+    render: (h) => h(App),
+}).$mount('#app');
