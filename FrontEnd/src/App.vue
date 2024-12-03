6724df106025c0e0131ea54b904ffbf65094e513
Francesco-Buscardo <francescobuscardo@gmail.com>
2024-12-03 17:08:33 +0100

    footer hedear 1

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -12,20 +12,36 @@ export default {
 
 <template>
 
-  <div>
-    <router-view />
+  <div id="App" class="container">
+    <header class="header">Pippo</header>
+    <main class="main">
+      <router-view />
+    </main>
+    <footer class="footer">Pluto</footer>
   </div>
-
-  <!--<div class="login">
-
-    <TITLE />
-    <Login />
-    <BTN_SIGN_UP />
-
-  </div>-->
 
 </template>
 
 <style scoped>
+
+  .container {  display: grid;
+    width: 100%;
+    height: 100%;
+    grid-template-columns: 1fr;
+    grid-template-rows: 0.2fr 2.6fr 0.2fr;
+    grid-auto-columns: 1fr;
+    gap: 0px 0px;
+    grid-auto-flow: row;
+    grid-template-areas:
+      "header"
+      "main"
+      "footer";
+  }
+
+  .header { grid-area: header; }
+
+  .footer { grid-area: footer; }
+
+  .main { grid-area: main; }
 
 </style>
