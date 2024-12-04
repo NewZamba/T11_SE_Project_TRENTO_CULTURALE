b4adecbf65f353f036094f544450fdece5828c4d
Francesco-Buscardo <francescobuscardo@gmail.com>
2024-12-04 15:04:06 +0100

    user home 2

---
+++ FrontEnd/src/components/Footer.vue
@@ -0,0 +1,29 @@
+<script>
+
+export default {
+  name: 'Footer',
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {}
+};
+
+</script>
+
+<template>
+  <div class="footer">
+    @Università di Trento - Gruppo: T11 - F.Buscardo, F.Cocquio, G.Marasca
+  </div>
+</template>
+
+<style scoped>
+
+  .footer {
+    text-align: center;
+    color: #53a199;
+    font-family: Helvetica, sans-serif;
+    font-size: small;
+  }
+
+</style>
---
+++ FrontEnd/src/components/User/Cards.vue
@@ -0,0 +1,60 @@
+<script>
+
+  export default {
+    name: 'Cards',
+    components: {},
+    data() {
+      return {};
+    },
+    props: {
+      events: {
+        type: Array,
+        required: true
+      }
+    }
+  };
+
+</script>
+
+<template>
+    <!-- Itera attraverso gli eventi per generare le carte -->
+    <b-card-group deck>
+      <b-card
+          v-for="event in events"
+          :key="event._id"
+          :title="event.name_event"
+          :img-src="event.img_event"
+          img-alt="Event Image"
+          img-top
+          tag="article"
+          class="card-item d-flex flex-column justify-content-between"
+      >
+        <b-card-text>
+          {{ event.description }}
+        </b-card-text>
+
+        <b-button href="#" variant="primary">View details</b-button>
+      </b-card>
+    </b-card-group>
+</template>
+
+<style scoped>
+
+  .card-item {
+    max-width: 20rem;
+    height: 100%;
+    flex: 1 1 calc(33.333% - 1rem);
+    box-sizing: border-box;
+    margin: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  @media (max-width: 768px) {
+    .card-item {
+      flex: 1 1 100%;
+    }
+  }
+
+</style>
