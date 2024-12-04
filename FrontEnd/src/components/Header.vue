48f78b393c1d484197b63a2cb1434929c2153839
Francesco-Buscardo <francescobuscardo@gmail.com>
2024-12-04 15:04:06 +0100

    user home 2

---
+++ FrontEnd/src/components/Header.vue
@@ -0,0 +1,29 @@
+<script>
+
+export default {
+  name: 'Header',
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {}
+};
+
+</script>
+
+<template>
+  <div class="header">
+      Trento Culturale
+  </div>
+</template>
+
+<style scoped>
+
+  .header {
+    text-align: center;
+    color: #53a199;
+    font-family: Helvetica, sans-serif;
+    font-size: small;
+  }
+
+</style>
---
+++ FrontEnd/src/components/User/Carousel.vue
@@ -0,0 +1,64 @@
+<script>
+
+  export default {
+    name: 'Carousel',
+    components: {},
+    data() {
+      return {
+        slide: 0,
+        sliding: null
+      };
+    },
+    props: {
+      events: {
+        type: Array,
+        required: true
+      }
+    },
+    methods: {
+
+    }
+  };
+
+</script>
+
+<template>
+
+    <b-carousel
+        id="carousel-1"
+        v-model="slide"
+        :interval="4000"
+        controls
+        img-height="30vh"
+        img-width="100vw"
+        style="text-shadow: 1px 1px 2px #333;"
+    >
+
+
+      <!--
+      Per le immagini le mettiamo in base64 nel database e poi ssi richiama la funz inversa per riportarle im img
+      -->
+      <b-carousel-slide
+          v-for="event in events"
+          :key="event._id"
+          :caption="event.name_event"
+          :img-src="event.img_event"
+          :img-width="1600"
+          :img-height="480"
+      >
+      </b-carousel-slide>
+    </b-carousel>
+
+</template>
+
+<style scoped>
+#carousel-1 {
+  height: 100%;
+  width: 60%;
+  align-items: center;
+}
+
+
+
+
+</style>
