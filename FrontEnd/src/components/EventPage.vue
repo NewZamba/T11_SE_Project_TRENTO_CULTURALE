57c7b61f75f518f591a0b6ca346efc8e07ec15eb
Francesco-Buscardo <francescobuscardo@gmail.com>
2024-12-05 13:43:14 +0100

    event page 2

--- FrontEnd/src/components/EventPage.vue
+++ FrontEnd/src/components/EventPage.vue
@@ -23,19 +23,103 @@
       this.date_event = this.$route.query.date_event;
       this.tags = this.$route.query.tags;
     },
-    methods: {}
+    methods: {
+      backToHome() {
+        this.$router.push('/UserHome');
+      }
+    }
   };
 
 </script>
 
 <template>
 
-  <div>
+  <div class="event-page">
+    <!--
+      bottone indietro
+     -->
+    <header class="header">
+      <button class="back_btn" @click="backToHome"></button>
+    </header>
+    <!--
+      dettagli evento
+    -->
+    <div class="event">
+      <div class="details_img">
+        <div class="details">
+          <div class="info">NAME: {{ name_event }}</div>
+          <div class="info">TAG: {{ tags }}</div>
+          <div class="info">DATE: {{ date_event }}</div>
+          <div class="info">LOCATION: {{ location_event }}</div>
+        </div>
+        <div class="img">
+          <img />
+        </div>
+      </div>
+      <div class="description">
+        DESCRIPTION:
+        <p>{{ description_event }}</p>
+      </div>
 
+    </div>
   </div>
 
 </template>
 
 <style scoped>
+
+  .event-page {
+    display: flex;
+    width: 40%;
+    height: 80%;
+    border-radius: 20px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    margin: auto auto;
+  }
+
+  .header {
+    justify-items: left;
+    padding: 5px;
+  }
+
+  .back_btn {
+    width: 45px;
+    height: 45px;
+    border: none;
+    border-radius: 50%;
+    background-color: #4cafa0;
+    color: white;
+    font-size: 16px;
+    line-height: 100px;
+    cursor: pointer;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
+    transition: transform 0.2s;
+  }
+
+  .back_btn:hover {
+    transform: scale(1.1);
+  }
+
+  .details_img img {
+
+  }
+
+  .details {
+
+  }
+
+  .img {
+
+  }
+
+  .description {
+
+  }
+
+  .info {
+    border-radius: 30px;
+    text-align: center;
+    padding: 5px;
+  }
 
 </style>
