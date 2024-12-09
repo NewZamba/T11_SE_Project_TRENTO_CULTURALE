83a8038dbeaa2bbff238f34431eabc71e5e9131c
Francesco-Buscardo <francescobuscardo@gmail.com>
2024-12-09 15:06:18 +0100

    event page 2.5.1 - prenotazione eventi

---
+++ FrontEnd/src/components/EventPage/Description.vue
@@ -0,0 +1,42 @@
+<script>
+
+  export default {
+    name: 'Description',
+    components: {},
+    data() {
+      return {
+        description_event: ''
+      };
+    },
+    mounted() {
+      this.description_event = this.$route.query.description_event;
+    },
+    methods: {}
+  };
+
+</script>
+
+<template>
+
+  <div class="description">
+    <h1>Description</h1>
+    <p>{{ description_event }}</p>
+  </div>
+
+</template>
+
+<style scoped>
+  .description {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    width: 100%;
+    text-align: left;
+    padding: 2% 2% 2% 4%;
+  }
+
+  h1 {
+    font-family: 'Helvetica', sans-serif;
+    font-size: x-large;
+  }
+</style>
