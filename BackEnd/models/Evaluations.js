db828261f648dd78c2474553851e75a7e48bd442
NEWZA <francesco.cocquio@barsanti.edu.it>
2025-01-31 17:38:30 +0100

    Aggiunta valutazione con star ai singoli eventi, con aggiornamento valore e creazione api di aggiunta ricezione e modifca del dato

--- BackEnd/models/Evaluations.js
+++ BackEnd/models/Evaluations.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const evaluation_schema = new Schema({
-    comment: {type: String, required: true},
     id_event: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Event',
         required: true},
+    id_user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
     rating: {
         type: Number,
         required: true
