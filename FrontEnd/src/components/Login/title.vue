adea61a179ba8910f561ad4db02670b17b035163
Francesco-Buscardo <francescobuscardo@gmail.com>
2025-02-03 16:23:39 +0100

    fix title

--- FrontEnd/src/components/Login/title.vue
+++ FrontEnd/src/components/Login/title.vue
@@ -15,12 +15,17 @@
 </script>
 
 <template>
-  <button class="slide">TRENTO CULTURALE</button>
+
+  <button>
+    TRENTO CULTURALE
+  </button>
+
 </template>
 
-<style scoped lang="scss">
-button {
-  height: 100px;
+<style scoped>
+
+  button {
+    height: 100px;
     width: 400px;
     padding: 2%;
     position: relative;
@@ -28,37 +33,10 @@ button {
     border: 3px solid white;
     border-radius: 20px;
     background: steelblue;
-
-    transition: all 1000ms;
-    z-index: 1;
-    overflow: hidden;
-
-    font-family: 'Helvetica';
+    font-family: 'Helvetica', serif;
     font-weight: bold;
     font-size: x-large;
     color: white;
-
-    &:hover {
-      border-color: steelblue;
-      background-color: white;
-      color: steelblue;
-    }
-      
-    &:after {
-      content: "";
-      position: absolute;
-      z-index: -1;
-      top: 0; bottom: 0; left: 0; right: 0;
-      transition: all 300ms;
-      background-color: white;
-    }
   }
 
-  .slide::after {
-    height: 100%;
-    width: 0;
-  }
-  .slide:hover::after {
-    width: 100%;
-  }
 </style>
