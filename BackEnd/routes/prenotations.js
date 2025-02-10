34a236760fa3c68dcc8fc2f69e3a2f97939f8911
NEWZA <francesco.cocquio@barsanti.edu.it>
2025-02-10 23:26:22 +0100

    Aggiunta Modal Global, e fix prenotazioni

--- BackEnd/routes/prenotations.js
+++ BackEnd/routes/prenotations.js
@@ -2,18 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Prenotation = require("../models/Prenotations");
 
-/* GET users listing. */
-router.get('/', async function (req, res, next) {
-
-    const lstPrenotations = await Prenotation.find();
-
-    if (lstPrenotations) {
-        return res.status(200).json(lstPrenotations);
-    } else {
-        return res.status(404).json({message: 'No prenotation found'});
-    }
-});
-
 router.get('/:id', async function (req, res, next) {
 
     try{
@@ -31,5 +19,18 @@ router.get('/:id', async function (req, res, next) {
         return res.status(500).json({ message: 'Errore del server.' });
     }
 });
+/* GET users listing. */
+router.get('/', async function (req, res, next) {
+
+    const lstPrenotations = await Prenotation.find();
+
+    if (lstPrenotations) {
+        return res.status(200).json(lstPrenotations);
+    } else {
+        return res.status(404).json({message: 'No prenotation found'});
+    }
+});
+
+
 
 module.exports = router;
